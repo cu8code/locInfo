71656077947477ccdea3fc61bf09518e757abbdf
Bishakh Dutta <bishakhdutta2002@gmail.com>
2022-12-31 23:39:45 +0530

    added zoom

--- components/editorCanvas/index.jsx
+++ components/editorCanvas/index.jsx
@@ -3,48 +3,44 @@
   * show the preview in real time
 * */
 
-import { useRef } from "react"
+import { useRef, useEffect } from "react"
 
 let currentZoomValue = 1
 const zoomDifferenceValue = 0.05
-
-
-/*
-  * when we zoom! we increse / decrease the size of the editorCanvasContent
-  * when we pan we move the editorCanvasContent left and right
-  * */
-
-function zoomin(editorCanvas) {
-  if (editorCanvas === null) return
-  // console.log("zoomin")
-  currentZoomValue += zoomDifferenceValue
-  editorCanvas.current.style['transform'] = `scale(${currentZoomValue})`
-}
-
-function zoomout(editorCanvasContent) {
-  if (editorCanvasContent === null) return
-  // console.log("zoomout")
-  currentZoomValue -= zoomDifferenceValue
-  editorCanvasContent.current.style['transform'] = `scale(${currentZoomValue})`
-}
-
 function pan(editorCanvasContent, event) {
   if (editorCanvasContent === null) return
   console.log(event.target)
 }
 
+
 export default function EditorCanvas() {
   const editorCanvasContent = useRef(null)
+
+  // zoom useeffect
+  
+  useEffect(() => {
+    const element = editorCanvasContent.current;
+    const zoom = event => {
+      event.preventDefault();
+      let zoomVal = Math.sign(event.deltaY);
+      if(zoomVal==1){
+        currentZoomValue -= zoomDifferenceValue;
+      }else{
+        currentZoomValue += zoomDifferenceValue;
+      }
+      element.style.transform = `scale(${currentZoomValue})`;
+    }
+    console.log(element);
+    element.addEventListener("wheel",zoom)
+    return () => {
+      element.removeEventListener("wheel",zoom)
+    };
+  }, []);
   return <>
     {/* editroCanvas */}
-    <div onScroll={() => console.log("editroCanvas")} className="fixed top-0 left-0 h-screen w-screen bg-slate-500 overflow-auto">
+    <div onScroll={() => console.log("editroCanvas")} className="top-0 left-0 h-screen w-screen bg-slate-500 overflow-auto flex justify-center">
       {/* this main elemet will be the representation of pdf page */}
-      <main ref={editorCanvasContent} className="bg-white transition duration-20"> CANVAS </main>
-    </div>
-    {/* zoom control button that will be placed at the end of the screen */}
-    <div className="fixed w-screen h-screen top-0 left-0 flex justify-center">
-      <button type="button" onClick={() => zoomin(editorCanvasContent)}>zoomin</button>
-      <button type="button" onClick={() => zoomout(editorCanvasContent)}>zoomout</button>
+      <main ref={editorCanvasContent} className="fixed w-2/5 h-5/6 bg-white transition duration-100 ease-in-out"> CANVAS </main>
     </div>
   </>
 }
