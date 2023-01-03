50170ed608edbe489365b7cb6f1f6080432440cc
Bishakh Dutta <bishakhdutta2002@gmail.com>
2023-01-03 20:51:28 +0530

    going great

--- components/editorCanvas/index.jsx
+++ components/editorCanvas/index.jsx
@@ -1,129 +1,28 @@
-/*
- * this cpmponent responsibe for create the main canvas over which we will
- * show the preview in real time
- * */
+import { useEffect, useRef, useState } from "react";
 
-import { useRef, useEffect, useState } from "react";
-
-let currentZoomValue = 0.6;
-const zoomDifferenceValue = 0.05;
-let pages = 1;
-
-// for pan
-let mouseDown = false; //if mouse is clicked
-let canvasOffset = [0, 0];
-
-// checking if canvas is out of viewport
-// const isInViewport = (element) => {
-//   const rect = element.getBoundingClientRect();
-//   return rect.top >= 0 && rect.bottom <= window.innerHeight;
-// };
-
-const pan = (e, element) => {
-  e.preventDefault();
-  const rect = element.getBoundingClientRect();
-  //if mouse is clicked and canvas is out of viewport due to zoom
-  if (mouseDown /*&& !isInViewport(element)*/) {
-    // stack overflow :)
-    element.style.top = e.clientY + canvasOffset[1] + "px";
-    element.style.left = e.clientX + canvasOffset[0] + "px";
-  }
-  // console.log(rect.top<=);
-};
-
-
-const Page = () => {
-  return (
-    <main
-      style={{
-        width: "210mm",
-        height: "297mm",
-      }}
-      className="bg-white transition duration-100 ease-in-out"
-    >
-      {" "}
-      CANVAS{" "}
-    </main>
+const Page = ({num})=>{
+  return(
+    <>
+      <div className="bg-white w-[70mm] h-[90mm]">
+      </div>
+      <div className="text-slate-50 bg-slate-700 text-xs text-right h-[1rem] mt-[-0.5rem]">Page: {num}</div>
+    </>
   );
 }
-
-const zoom = (event, maxZoom, minZoom, canvas) => {
-  event.preventDefault();
-  if (canvas === null) return
-  let zoomVal = Math.sign(event.deltaY);
-  if (zoomVal == 1 && currentZoomValue > minZoom) {
-    currentZoomValue -= zoomDifferenceValue;
-  } else if (zoomVal == -1 && currentZoomValue < maxZoom) {
-    currentZoomValue += zoomDifferenceValue;
-  }
-  console.log(event.x, event.y, ",", canvas.current.offsetLeft);
-  canvas.current.style.transform = `scale(${currentZoomValue})`;
-};
-
-const addPages = (pagesArray, setPages, canvas) => {
-  pages++;
-  setPages([...pagesArray, { key: pages }]);
-  console.log(canvas.current.style.left)
-}
-
-export default function EditorCanvas({ maxZoom, minZoom }) {
-  const [pagesArray, setPages] = useState([{ key: 1 }])
-  const canvas = useRef(null);
-  const canvasContainer = useRef(null);
-
-  useEffect(() => {
-    const element = canvasContainer.current;
-    //event listeners for zoom and pan
-    element.addEventListener("wheel", (e) => zoom(e, maxZoom, minZoom, canvas));
-
-    element.addEventListener("mousedown", (e) => {
-      e.preventDefault();
-      mouseDown = true;
-      // logic from stack overflow
-      canvasOffset = [
-        canvas.current.offsetLeft - e.clientX,
-        canvas.current.offsetTop - e.clientY,
-      ];
-    });
-    element.addEventListener("mouseup", () => {
-      mouseDown = false;
-    });
-    element.addEventListener("mousemove", (e) => {
-      pan(e, canvas.current);
-    });
-    return () => {
-      element.removeEventListener("wheel", (e) => zoom(e, maxZoom, minZoom, canvas));
-    };
-  }, []);
-  return (
+export default function EditorCanvas({zoomVal,pages}){
+  return(
     <>
-      {/* editroCanvas */}
-      <div
-        onScroll={() => console.log("editroCanvas")}
-        style={{ background: "#171717" }}
-        ref={canvasContainer}
-        className="overflow-hidden top-0 left-0 h-screen w-screen flex justify-center items-center"
-      >
-        {/* this main elemet will be the representation of pdf page */}
+      <div className="w-full min-h-full h-fit flex justify-center">
         <div
-          ref={canvas}
-          style={{
-            transform: "scale(0.6)",
-            minWidth: "210mm",
-            minHeight: "210mm",
-            left: "35vw",
-            transformOrigin: "0% center"
-          }}
-          className="w-fit h-fit fixed gap-8 flex flex-row"
-        >
-          {pagesArray.map((data) => {
-            return (
-              <Page key={data.key} />
-            )
+        style={{transform:`scale(${zoomVal})`}}
+         className="pt-4 pb-4 w-fit h-fit flex flex-col gap-2 origin-top duration-200">
+          {pages.map((data)=>{
+            return(
+              <Page key={data.key} num={data.key}/>
+            );
           })}
         </div>
       </div>
-      <button className="fixed top-0 bg-white" onClick={() => addPages(pagesArray, setPages, canvas)}>Add page</button>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import EditorCanvas from "../components/editorCanvas"
+import { useState } from "react";
+import EditorCanvas from "../components/editorCanvas";
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+
+let totalPages = 1;
 
 export default function Home() {
+  const [theme,setTheme]=useState("white");
+  const [zoom,setZoom]=useState(1.5);
+  const [pages,setpages]=useState([{key:1}]);
+  const handelZoom=(zoom)=>{
+    if(zoom>=1&zoom<=3) setZoom(zoom);
+  }
+  const addPages=()=>{
+    totalPages++;
+    setpages([...pages,{key:totalPages}])
+  }
   return (
     <>
-      <EditorCanvas maxZoom={2} minZoom={0.3} />
+      <div className="flex flex-col w-full h-screen overflow-hidden">
+        <nav className="w-full h-24 bg-slate-700 min-h-[6rem]">
+          <button onClick={()=>handelZoom(zoom+0.5)}><ZoomInIcon style={{color:theme}}/></button>
+          <button onClick={()=>handelZoom(zoom-0.5)}><ZoomOutIcon style={{color:theme}}/></button>
+          <button onClick={addPages}><NoteAddIcon style={{color:theme}}/></button>
+        </nav>
+        <div className="flex flex-1 h-[calc(100vh-6rem)]">
+          <div className="bg-slate-800 w-52"></div>
+          <div className="bg-gray-800 flex-1 overflow-y-auto scrollbar-none scroll-smooth">
+            <EditorCanvas zoomVal={zoom} pages={pages}/>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
